1450284df032c13494a8338ba9e4561fd2b5f980
lockwood-ethan <lockwood.ethan@yahoo.com>
2025-11-08 17:42:38 +0000

    passing first serialization test

---
+++ backend/src/main/java/backend/blog/entities/Post.java
@@ -0,0 +1,4 @@
+package backend.blog.entities;
+
+public record Post(Long id, String title, String text) {
+}
---
+++ backend/src/test/java/backend/blog/PostJsonTests.java
@@ -0,0 +1,30 @@
+package backend.blog;
+
+import backend.blog.entities.Post;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class PostJsonTests {
+
+    @Autowired
+    private JacksonTester<Post> json;
+
+    @Test
+    void postSerializationTest() throws IOException {
+        Post post = new Post(23L, "Test Title", "Test Text");
+        assertThat(json.write(post)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(post)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(post)).extractingJsonPathNumberValue("@.id").isEqualTo(23);
+        assertThat(json.write(post)).hasJsonPathStringValue("@.title");
+        assertThat(json.write(post)).extractingJsonPathStringValue("@.title").isEqualTo("Test Title");
+        assertThat(json.write(post)).hasJsonPathStringValue("@.text");
+        assertThat(json.write(post)).extractingJsonPathStringValue("@.text").isEqualTo("Test Text");
+    }
+}
